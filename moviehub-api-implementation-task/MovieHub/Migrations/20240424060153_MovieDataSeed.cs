006831527789db650810fcb573f48b482da913b5
Lukasz Golebiowski <lukasz.golebiowski@lexicondigital.com.au>
2024-04-24 16:09:48 +1000

    run movie data seed migration

---
+++ moviehub-api-implementation-task/MovieHub/Migrations/20240424060153_MovieDataSeed.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieHub.Migrations
+{
+    /// <inheritdoc />
+    public partial class MovieDataSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            string sqlFilePath = "../db/moviehub-db-data-seed.sql";
+            string sqlContent = File.ReadAllText(sqlFilePath);
+            migrationBuilder.Sql(sqlContent);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
