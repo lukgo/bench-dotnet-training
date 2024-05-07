551ac9ddc6de4f50aaff31f2cef214bdd9f835c3
Lukasz Golebiowski <lukasz.golebiowski@lexicondigital.com.au>
2024-05-07 16:53:26 +1000

    feat: add movie deatils enpoints and resolvers

--- moviehub-api-implementation-task/MovieHub/Controllers/MovieController.cs
+++ moviehub-api-implementation-task/MovieHub/Controllers/MovieController.cs
@@ -1,11 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
+using MovieHub.Data;
 using MovieHub.Models;
 using MovieHub.Services;
 
 namespace MovieHub.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api")]
 public class MovieController : ControllerBase
 {
     readonly MovieService _service;
@@ -18,15 +19,13 @@ public class MovieController : ControllerBase
     [HttpGet]
     public IEnumerable<Movie> GetAll() => _service.GetAll();
 
-    [HttpGet("~/get-movie-cinemas")]
-    public IEnumerable<MovieCinema> GetMovieCinemas() => _service.GetMovieCinemas();
-
     [HttpGet("~/get-movie/title/{title}")]
-    public IEnumerable<Movie> GetMovieByTitle(string title) => _service.GetMovieByTitle(title);
+    public IEnumerable<Movie> GetMovieByTitle(string title) => _service.GetMoviesByTitle(title);
 
     [HttpGet("~/get-movie/genre/{genre}")]
-    public IEnumerable<Movie> GetMovieByGenre(string genre) => _service.GetMovieByGenre(genre);
+    public IEnumerable<Movie> GetMovieByGenre(string genre) => _service.GetMoviesByGenre(genre);
 
-    [HttpGet("~/get-cinemas")]
-    public IEnumerable<Cinema> GetCinemas() => _service.GetCinemas();
+    [HttpGet("~/get-cinemas/{movieId}")]
+    public IEnumerable<Cinema> GetMovieDetailsWithCinemas(int movieId) =>
+        _service.GetCinemasWithMovie(movieId);
 }
--- moviehub-api-implementation-task/MovieHub/Program.cs
+++ moviehub-api-implementation-task/MovieHub/Program.cs
@@ -1,6 +1,6 @@
+using Microsoft.OpenApi.Models;
 using MovieHub.Data;
 using MovieHub.Services;
-using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,27 +10,29 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSqlite<MovieContext>("Data Source=MovieContext.db");
 builder.Services.AddSwaggerGen(options =>
 {
-    options.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Version = "v1",
-        Title = "ToDo API",
-        Description = "An ASP.NET Core Web API for managing ToDo items",
-        TermsOfService = new Uri("https://example.com/terms"),
-        Contact = new OpenApiContact
+    options.SwaggerDoc(
+        "v1",
+        new OpenApiInfo
         {
-            Name = "Example Contact",
-            Url = new Uri("https://example.com/contact")
-        },
-        License = new OpenApiLicense
-        {
-            Name = "Example License",
-            Url = new Uri("https://example.com/license")
+            Version = "v1",
+            Title = "ToDo API",
+            Description = "An ASP.NET Core Web API for managing ToDo items",
+            TermsOfService = new Uri("https://example.com/terms"),
+            Contact = new OpenApiContact
+            {
+                Name = "Example Contact",
+                Url = new Uri("https://example.com/contact")
+            },
+            License = new OpenApiLicense
+            {
+                Name = "Example License",
+                Url = new Uri("https://example.com/license")
+            }
         }
-    });
+    );
 });
 builder.Services.AddScoped<MovieService>();
 builder.Services.AddControllers();
-// builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -38,7 +40,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(options => {
+    app.UseSwaggerUI(options =>
+    {
         options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
         options.RoutePrefix = string.Empty;
     });
@@ -49,28 +52,16 @@ app.UseHttpsRedirection();
 
 var summaries = new[]
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    "Freezing",
+    "Bracing",
+    "Chilly",
+    "Cool",
+    "Mild",
+    "Warm",
+    "Balmy",
+    "Hot",
+    "Sweltering",
+    "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
