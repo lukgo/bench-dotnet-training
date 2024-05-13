460bb6ab2185b96f27650926a76defde838923dc
Lukasz Golebiowski <lukasz.golebiowski@lexicondigital.com.au>
2024-05-13 15:33:20 +1000

    feat: add averageScore

--- moviehub-api-implementation-task/MovieHub/Models/Movie.cs
+++ moviehub-api-implementation-task/MovieHub/Models/Movie.cs
@@ -25,4 +25,33 @@ public class Movie
     public string princessTheatreMovieId { get; set; }
     public ICollection<MovieReview>? MovieReviews { get; set; }
     public ICollection<MovieCinema>? MovieCinema { get; set; }
+
+    public class MovieWithAverageScore()
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public DateOnly releaseDate { get; set; }
+        public string genre { get; set; }
+        public int runtime { get; set; }
+        public string synopsis { get; set; }
+        public string director { get; set; }
+        public string rating { get; set; }
+        public string princessTheatreMovieId { get; set; }
+        public double averageScore { get; set; }
+
+        public MovieWithAverageScore(Movie movie, double averageScore)
+            : this()
+        {
+            id = movie.id;
+            title = movie.title;
+            releaseDate = movie.releaseDate;
+            genre = movie.genre;
+            runtime = movie.runtime;
+            synopsis = movie.synopsis;
+            director = movie.director;
+            rating = movie.rating;
+            princessTheatreMovieId = movie.princessTheatreMovieId;
+            averageScore = averageScore;
+        }
+    }
 }
